99d2d9f51cb4b005a536472234b1406cac4cc4fb
szezhen.wong <szezhen94@gmail.com>
2019-06-20 00:46:23 +0800

    Enhance CORS

--- server/server.js
+++ server/server.js
@@ -38,7 +38,16 @@ mongoose
   .catch(err => console.log(err));
 
 // CORS middleware
-app.use(cors({ origin: "https://confident-darwin-f47339.netlify.com" }));
+const whitelist = ["https://confident-darwin-f47339.netlify.com"];
+const corsOptionsDelegate = function(req, callback) {
+  let corsOptions;
+  if (whitelist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
 // app.use(function(req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
 //   res.header("Access-Control-Allow-Credentials", true);
